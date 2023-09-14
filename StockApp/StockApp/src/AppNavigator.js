aa2a485a864728c65cf84703947b9f923002b692
mmi-ing <tpgns252@gmail.com>
2023-09-15 01:43:29 +0900

    네비게이터에 커뮤페이지 추가

--- StockApp/StockApp/src/AppNavigator.js
+++ StockApp/StockApp/src/AppNavigator.js
@@ -3,10 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import MainPage from './pages/MainPage';
 import OtherPage from './pages/OtherPage'; 
+import CommunityPage from './pages/CommunityPage'
+import { BoardDetail } from './pages/BoardDetail';
+
 
 
 const Stack = createStackNavigator();
@@ -14,9 +18,10 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Main" component={MainPage} />
+    <Tab.Navigator initialRouteName='Main'>
       <Tab.Screen name="Other" component={OtherPage} />
+      <Tab.Screen name="Main" component={MainPage} />
+      <Tab.Screen name="Community" component={CommunityPage} />
       {/* 필요한 만큼 탭 추가 */}
     </Tab.Navigator>
   );
@@ -28,7 +33,8 @@ function AppNavigator() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="SignIn" component={SignInPage} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="MainStack" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="BoardDetail" component={BoardDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
