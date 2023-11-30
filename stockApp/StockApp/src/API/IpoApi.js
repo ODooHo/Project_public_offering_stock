31a5e2c2930f3955f05fc67928180399cf563bd1
mmi-ing <tpgns252@gmail.com>
2023-12-01 05:19:44 +0900

    IpoApi에 리프래시 토큰 로직 추가

--- stockApp/StockApp/src/API/IpoApi.js
+++ stockApp/StockApp/src/API/IpoApi.js
@@ -1,21 +1,9 @@
 import axios from "axios";
-import { getToken } from "../tokenManager";
-
-const SERVER_URL = 'http://15.165.24.146:8080';
-
-const getAuthToken = async () => {
-    const token = await getToken();
-    if (token) {
-        // console.log("가져온 토큰:",token);
-        return `Bearer ${token}`;
-    }
-    return null;
-};
+import { SERVER_URL, makeAuthenticatedRequest } from "../tokenManager";
 
 export const fetchIpoList = async () => {
   try {
     const response = await axios.get(`${SERVER_URL}/api/stock/list`);
-    console.log("공모주정보", response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetchin IPO list:', error);
@@ -25,9 +13,8 @@ export const fetchIpoList = async () => {
 
 export async function fetchIpoDetail(ipoName) {
   try {
-    console.log(`Fetching IPO details for: ${ipoName}`); // 로그 추가
+    console.log(`종목 상세 정보 가져오기: ${ipoName}`);
     const response = await axios.get(`${SERVER_URL}/api/stock/${ipoName}`);
-    // const response = await axios.get(`${SERVER_URL}/api/stock/detail/${stockId}`);
     console.log(response.data.data)
     return response.data.data;
   } catch (error) {
@@ -39,18 +26,7 @@ export async function fetchIpoDetail(ipoName) {
 //관심
 export const addFavoriteIpo = async (ipoName, userEmail) => {
   try {
-    console.log("Add IPO with name(API):", ipoName);
-    const token = await getAuthToken();
-    const response = await axios.post(`${SERVER_URL}/api/stock/addFavor`, {
-      ipoName,
-      userEmail
-    }, {
-      headers: {
-        'Authorization': token
-      }
-    });
-    console.log(response.data);
-    return response.data;
+    return await makeAuthenticatedRequest('POST', `/api/stock/addFavor`, { ipoName, userEmail });
   } catch (error) {
     console.error('Error adding IPO to favorites(IpoApi):', error);
     throw error;
@@ -59,30 +35,16 @@ export const addFavoriteIpo = async (ipoName, userEmail) => {
 
 export const deleteFavoriteIpo = async (ipoName) => {
   try {
-    console.log("Removing IPO with name(API):", ipoName);
-    const response = await axios.delete(`${SERVER_URL}/api/stock/${ipoName}/deleteFavor`, {
-      headers: {
-        'Authorization': await getAuthToken()
-      }
-    });
-    console.log("Deleted IPO response:", response.data);
-    return response.data;
+    return await makeAuthenticatedRequest('DELETE', `/api/stock/${ipoName}/deleteFavor`);
   } catch (error) {
-    console.error('Error response from server:', error.response);
+    console.error('Error deleting favorite IPO:', error);
     throw error;
   }
 };
 
 export const getFavoriteIpo = async () => {
   try {
-    const token = await getAuthToken();
-    const response = await axios.get(`${SERVER_URL}/api/stock/getFavor`, {
-      headers: {
-        'Authorization': token
-      }
-    });
-    console.log("관심종목리스트:", response.data);
-    return response.data;
+    return await makeAuthenticatedRequest('GET', '/api/stock/getFavor');
   } catch (error) {
     console.error('Error fetching favorite IPOs:', error);
     throw error;
@@ -103,17 +65,9 @@ export const searchIpo = async (searchDto) => {
 
 export const fetchRecentSearches = async () => {
   try {
-    const autoken = await getAuthToken();
-    const response = await axios.get(`${SERVER_URL}/api/search/stock`, {
-      headers: {
-        'Authorization': autoken
-      }
-    });
-    console.log("최근 검색어(공모주):", response.data);
-    return response.data;
+    return await makeAuthenticatedRequest('GET', '/api/search/stock');
   } catch (error) {
-    console.error("최근 검색어(공모주)를 가져오는 중 오류 발생:", error);
+    console.error("Error fetching recent searches:", error);
     throw error;
   }
 };
-
